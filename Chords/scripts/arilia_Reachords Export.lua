7a4ecbba5101b46b2e0d2e4de1e6b7d5a8e5a3ff
arilia <>
2026-08-10 17:39:57 +0200

    files added

---
+++ Chords/scripts/arilia_Reachords Export.lua
@@ -0,0 +1,241 @@
+reaper.ClearConsole()
+--reaper.ShowConsoleMsg("Starting...\n")
+
+
+local projectName = reaper.GetProjectName()
+projectName = projectName:match("(.+)%..+$") or projectName
+reaper.SetExtState("reachords", "project", projectName, false)
+--reaper.ShowConsoleMsg(projectName .. "\n")
+
+local globalOffset = reaper.GetProjectTimeOffset(0, false)
+--reaper.ShowConsoleMsg(offset .. "\n")
+
+local dirty = reaper.IsProjectDirty(0)
+--reaper.ShowConsoleMsg(dirty .. "\n")
+
+
+function getChordsJson(tr, chordsFound ) 
+	local chordjs = "{"
+	if chordsFound then
+		offset =  reaper.GetMediaTrackInfo_Value(tr, "D_PLAY_OFFSET")
+		local itemCount = reaper.CountTrackMediaItems(tr)
+		for i=1, itemCount do
+			  item = reaper.GetTrackMediaItem(tr, i-1)
+			  position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+			  length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+			  bpm = reaper.TimeMap_GetDividedBpmAtTime(position)
+			  endTime = position + length
+			  -- Converts time in beats
+			  duration = length * (bpm / 60)
+			  _, text = reaper.GetSetMediaItemInfo_String(item, "P_NOTES", '', false)
+			  
+			  local positionString = reaper.format_timestr_pos( position, '', 2 )
+			  
+			  measure, beat, sub = positionString:match("^(%d+)%.(%d+)%.(%d+)$")
+			  
+			  if measure then
+			      measure = tonumber(measure)
+			      beat    = tonumber(beat)
+			      sub     = tonumber(sub)
+			  end
+			  chordjs = chordjs .. '"' .. i .. '": { "text":'.. '"' .. text .. '"'
+			  chordjs = chordjs .. ', "barNumber":' .. measure
+			  chordjs = chordjs .. ', "beatStart":'.. beat;
+			  chordjs = chordjs .. ', "sub":'.. sub;
+			  chordjs = chordjs .. ', "beatDuration":'.. duration;
+			  chordjs = chordjs .. ', "startTime":'.. position;
+			  chordjs = chordjs .. ', "endTime":'.. endTime;
+			  chordjs = chordjs .. '}'
+			  if i<itemCount then
+				 chordjs = chordjs .. ','
+			  end
+		  
+		 end
+	end 
+	
+	chordjs = chordjs .. '}'
+	-- reaper.ShowConsoleMsg(chordjs .. "\n")
+	return chordjs
+end
+
+-------------------
+-- MARKERS LIST    
+-------------------
+
+local mark = "{";
+local markerCount = reaper.CountProjectMarkers(0)
+local markCount =0;
+for i=0, markerCount do 
+	retval, isrgn,  pos,  rgnend,  name,  markrgnindexnumber, color = reaper.EnumProjectMarkers3(0, i)
+  
+	if(isrgn or #name == 0) then
+	else
+		r=0
+		g=0
+		b=0
+			r, g, b = reaper.ColorFromNative(color)
+		markCount = markCount +1
+		if markCount>1 then
+		  mark = mark .. ','
+		end
+		local bar = reaper.format_timestr_pos( pos, '', 2 )
+		bar, _, _ = bar:match("^(%d+)%.(%d+)%.(%d+)$")
+		mark = mark .. '"' .. bar .. '": {' 
+		mark = mark .. '"barNumber":' .. bar
+		mark = mark .. ', "position": ' .. pos 
+		mark = mark .. ', "text": "' .. name .. '"'
+		mark = mark .. ', "color": "' .. r .. ", " .. g .. ", " .. b .. '"'
+		mark = mark .. '}'
+		
+	end
+  
+  end
+ mark = mark .. '}'
+
+
+--reaper.ShowConsoleMsg(mark .. "\n")
+
+-------------------
+-- MEASURES LIST    
+-------------------
+
+
+local projectLength = reaper.GetProjectLength()
+local lastBarString = reaper.format_timestr_pos( projectLength, '', 2 )
+lastMeasure, _, _ = lastBarString:match("^(%d+)%.(%d+)%.(%d+)$")
+if lastMeasure then 
+  lastMeasure = tonumber(lastMeasure)
+end
+
+local firstBarString = reaper.format_timestr_pos( 0, '', 2 )
+firstMeasure, _, _ = firstBarString:match("([^.]+)")
+if firstMeasure then 
+  firstMeasure = tonumber(firstMeasure)
+end
+local meas = "{";
+for m=firstMeasure, lastMeasure do
+
+	barStart, _, _, numOfBeats, _, BPM = reaper.TimeMap_GetMeasureInfo(0, m-1)
+	barEnd = reaper.TimeMap_GetMeasureInfo(0, m)
+	barStart = barStart - globalOffset
+	barEnd = barEnd - globalOffset
+	meas = meas .. '"' .. m .. '": {' 
+	meas = meas .. '"number":' .. m
+	meas = meas .. ', "startTime": "' .. barStart .. '"'
+	meas = meas .. ', "endTime": "' .. barEnd .. '"'
+	meas = meas .. ', "numOfBeats":' .. numOfBeats
+		
+	meas = meas .. '}'
+ if m<lastMeasure then
+    meas = meas .. ','
+  end
+  
+end
+meas = meas .. '}'
+
+
+
+
+
+-------------------
+-- CHORDS LIST    
+-------------------
+
+
+local trackCount = reaper.CountTracks(0);
+local trackNumber = 0
+
+-- FIND THE TRACK NAMED "chords" TODO make this string an option
+local chordsFound = false
+for i=1, trackCount do 
+  local tr = reaper.GetTrack(0,i-1)
+  local ok, trackName = reaper.GetTrackName(tr)
+  if (trackName == "Chords" or trackName == "chords") then
+    chordsFound = true
+    trackNumber = i-1
+  end
+end
+tr = reaper.GetTrack(0, trackNumber)
+
+local offset = 0
+
+--reaper.ShowConsoleMsg(offset)
+
+-- Iterates all the items in the track and create a JSON objet with the list of the chords
+
+local chordjs = "{}"
+chordjs  = getChordsJson(tr, chordsFound)
+
+--reaper.ShowConsoleMsg(chordjs .. "\n")
+-------------------
+-- LYRICS LIST    
+-------------------
+
+
+local trackNumber = 0
+local lyricsFound = false
+-- FIND THE TRACK NAMED "lyrics" TODO make this string an option
+for i=1, trackCount do 
+  local tr = reaper.GetTrack(0,i-1)
+  local ok, trackName = reaper.GetTrackName(tr)
+  if trackName == "lyrics" or trackName == "Lyrics" then
+    lyricsFound = true
+    trackNumber = i-1
+  end
+end
+tr = reaper.GetTrack(0, trackNumber)
+
+
+-- Iterates all the items in the track and create a JSON objet with the list of the chords
+
+local lyrjs = "{"
+if lyricsFound then
+	local itemCount = reaper.CountTrackMediaItems(tr)
+	for i=1, itemCount do
+	  item = reaper.GetTrackMediaItem(tr, i-1)
+	  position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+	  length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+	  bpm = reaper.TimeMap_GetDividedBpmAtTime(position)
+	  endTime = position + length
+	  -- Converts time in beats
+	  duration = length * (bpm / 60)
+	  _, text = reaper.GetSetMediaItemInfo_String(item, "P_NOTES", '', false)
+	  
+	  local positionString = reaper.format_timestr_pos( position, '', 2 )
+	  
+	 
+	  lyrjs = lyrjs .. '"' .. i .. '": { "text":'.. '"' .. text .. '"'
+	  lyrjs = lyrjs .. ', "duration":'.. duration;
+	  lyrjs = lyrjs .. ', "startTime":'.. position;
+	  lyrjs = lyrjs .. ', "endTime":'.. endTime;
+	  lyrjs = lyrjs .. '}'
+	  if i<itemCount then
+	    lyrjs = lyrjs .. ','
+	  end
+	  
+	end
+end
+lyrjs = lyrjs .. '}'
+
+
+--reaper.ShowConsoleMsg(lyrjs .. "\n")
+old_json = reaper.GetExtState("reachords", "chords")
+
+local json = "{"
+json = json .. '"chords":' .. chordjs
+json = json .. ', "lyrics":' .. lyrjs
+json = json .. ', "bars":' .. meas
+json = json .. ', "markers":' .. mark
+json = json .. ', "title": "' .. projectName .. '"'
+json = json .. ', "offset": "' .. offset .. '"'
+json = json .. ', "globalOffset": "' .. globalOffset .. '"'
+json = json .. '}'
+
+if json ~= old_json then 
+	reaper.SetExtState("reachords", "dirty", "true", false)
+	
+else
+	reaper.SetExtState("reachords", "dirty", "false", false)
+end
+reaper.SetExtState("reachords", "chords", json, false)
+
---
+++ Chords/web/js/chordslyrics.js
@@ -0,0 +1,677 @@
+class Beat {
+    bar = null
+    number = 1;
+    div = null;
+
+    constructor(bar) {
+        this.bar = bar;
+    }
+
+    set playing(val) {
+        if (val) {
+            this.div.classList.add('beat_playing');
+        } else {
+            this.div.classList.remove('beat_playing');
+        }
+    }
+
+    render() {
+        this.div = document.createElement('div');
+        this.div.classList.add('beat');
+        this.div.id = this.bar.number + "." + this.number;
+        this.bar.div.append(this.div);
+    }
+}
+
+
+
+class Bar {
+    song = null;
+    row = null;
+    div = null;
+    beats = [];
+    chords = [];
+    number = 1;
+    beatDuration = 4;
+    startTime = 0;
+    endTime = 0;
+    isPlaying = false;
+
+    constructor(b) {
+        this.number = b.number;
+        this.song = this;
+        this.startTime = Math.round(Number(b.startTime) * 10000) / 10000;
+        this.endTime = Math.round(Number(b.endTime) * 10000) / 10000;
+        this.beatDuration = b.numOfBeats;
+        this.createBeats();
+    }
+
+    createBeats() {
+        this.beats = [];
+        for (var b = 1; b <= this.beatDuration; b++)
+        {
+            var beat = new Beat(this);
+            beat.number = b;
+            this.beats.push(beat);
+        }
+    }
+
+    set playing(val) {
+        if (val) {
+            this.div.classList.add('playing');
+        } else {
+            this.div.classList.remove('playing');
+        }
+        if (val && !this.isPlaying) {
+
+            var row_box = document.getElementById('position_row').getBoundingClientRect()
+            var target_position = row_box.top + row_box.height / 2;
+            var positionInfo = this.div.getBoundingClientRect();
+            var top = positionInfo.top + positionInfo.height / 2;
+            this.song.translate(target_position - top);
+        }
+        this.isPlaying = val;
+    }
+
+    checkPlaying(position) {
+        this.playing = (position >= this.startTime && position < this.endTime);
+    }
+
+    get width() {
+        var left = this.beats[0].div.getBoundingClientRect().left;
+        var right = this.beats[this.beats.length - 1].div.getBoundingClientRect().right;
+        var width = right - left;
+        return width;
+    }
+
+    render() {
+        this.div = document.createElement('div');
+        this.div.classList.add('bar');
+        this.div.style.setProperty("--beats", this.beats.length);
+        var time_div = document.createElement('div');
+        time_div.classList.add('time');
+        time_div.innerHTML = this.number;
+        this.div.appendChild(time_div);
+        if(this.row.div) this.row.div.append(this.div); else console.log(this);
+        for (let beat of this.beats)
+        {
+            beat.render();
+        }
+    }
+}
+
+class Row {
+    bars = [];
+    div = null;
+    type = "";
+    song = "";
+    table = "";
+    number = 0;
+    elements = [];
+    static MARKER = "marker";
+    static LYRICS_MARKER = "lyrics_marker";
+    static CHORDS = "chord";
+    static LYRICS = "lyric";
+
+    append(element)
+    {
+        this.elements.push(element);
+        element.row = this;
+    }
+
+    constructor(song, type = Row.CHORDS) {
+        this.type = type;
+        this.song = song;
+        this.number = song.rows.lenght + 1;
+    }
+
+    render() {
+        this.div = document.createElement("div");
+        switch(this.type) {
+            case Row.CHORDS :
+                this.div.classList.add('bar_row');
+                break;
+            case Row.LYRICS:
+                this.div.classList.add('lyric_row');
+                break;
+            case Row.MARKER:
+                this.div.classList.add('marker_row');
+                break;
+            case Row.LYRICS_MARKER:
+                this.div.classList.add('lyric_marker_row');
+                break;
+        }
+        this.div.id = "row_" + this.number;
+        for (var element of this.elements) {
+            if(element !== undefined) {
+                element.render();
+            }
+        }
+        this.song.table.append(this.div);
+    }
+}
+
+
+class Marker {
+    text = "";
+    barNumber = 1;
+    position = 0;
+    color = '';
+    div = null;
+    row = null;
+
+    constructor(m) {
+        this.text = m.text;
+        this.color = m.color;
+        this.barNumber = m.barNumber;
+        this.position = m.position;
+    }
+    render() {
+        this.div = document.createElement("div");
+        this.div.innerHTML = this.text;
+        this.div.classList.add('marker');
+        this.div.style.border = "1px solid rgb(" + this.color + ")";
+        if(this.row.div) this.row.div.append(this.div);
+        
+    }
+}
+
+class Chord {
+    name = "C";
+    beatDuration = 4;
+    bar = null;
+    barNumber = 1;
+    beatStart = 1; //beat inside the bar
+    startTime;  //in seconds
+    endTime; //in seconds
+    duration; //in seconds
+    isPlaying = false;
+    div = null;
+    song = null;
+
+    constructor(c) {
+        this.startTime = Math.round(Number(c.startTime) * 10000) / 10000;
+        this.endTime = Math.round(Number(c.endTime) * 10000) / 10000;
+        this.barNumber = c.barNumber;
+        this.beatStart = c.beatStart;
+        this.beatDuration = c.beatDuration;
+        this.name = c.text;
+    }
+
+    set playing(val) {
+        this.isPlaying = val;
+        if (val && this.div !== null) {
+            this.div.classList.add('chord_playing');
+        } else {
+            this.div.classList.remove('chord_playing');
+        }
+    }
+
+    checkPlaying(position) {
+
+        if (position >= this.startTime && position < this.endTime) {
+            this.playing = true;
+        } else {
+            this.playing = false;
+        }
+    }
+
+    render()
+    {
+        this.div = document.createElement('div');
+        this.div.classList.add('chord');
+        this.div.innerHTML = this.name;
+        this.div.style.left = Number((this.beatStart - 1) * this.bar.width / this.bar.beats.length) + "px"
+        this.div.style.width = Number(this.beatDuration * this.bar.width / this.bar.beats.length) - 4 + "px"
+        this.bar.div.append(this.div);
+    }
+}
+
+
+
+
+class Lyric {
+    text = "Lyrics";
+    startTime;  //in seconds
+    endTime; //in seconds
+    duration; //in seconds
+    isPlaying = false;
+    div = null;
+    song = null;
+    row = null;
+    nextLyric = null;
+
+    constructor(c) {
+        this.startTime = c.startTime;
+        this.endTime = c.endTime;
+        this.duration = this.endTime - this.startTime;
+        this.text = c.text;
+    }   
+
+    set playing(val) {
+        if (val) {
+            this.div.classList.add('lyric_playing');
+        } else {
+            this.div.classList.remove('lyric_playing');
+        }
+        if (val) {
+
+            var div = this.div;
+            var row_box = document.getElementById('position_row').getBoundingClientRect();
+            
+            var target_position = row_box.top + row_box.height / 2;
+            
+            var positionInfo = this.div.getBoundingClientRect();
+            
+            var top = positionInfo.top;// + positionInfo.height/2;
+            var nextLyricPositionInfo = this.nextLyric.div.getBoundingClientRect();
+            var height = nextLyricPositionInfo.top - positionInfo.top;
+            var progress = height * (this.song.calculatedPosition - this.startTime) / this.duration;
+
+            this.song.moveTo(target_position - top - progress);
+        }
+        this.isPlaying = val;
+
+    }
+
+    render()
+    {
+        this.div = document.createElement('div');
+        this.div.classList.add('lyric');
+        this.div.innerHTML = this.text;
+        if (this.playing)
+        {
+            this.div.classList.add('playing')
+        }
+        this.row.div.append(this.div);
+    }
+
+    checkPlaying(position) {
+
+        if (position >= this.startTime && position < this.endTime) {
+
+            this.playing = true;
+
+        } else {
+
+            this.playing = false;
+
+        }
+    }
+}
+
+class Song {
+    id = 0;
+    table = null;
+    lastInsertedLyric = null;
+    bars = [];
+    chords = [];
+    lyrics = [];
+    rows = [];
+    json = "";
+    markers = [];
+    maxBeats = 16;
+    barsPerRow = 4;
+    translateY = 0;
+    offset = 0;
+    lastRecordedPosition = 0;
+    lastRecordedTime = 0;
+    calculatedPosition = 0;
+    transportStatus = 0;
+    globalOffset = 0;
+    type = 'lyrics';
+    projectTitle = "Song";
+    complete = false;
+    static LYRICS = "lirics";
+    static CHORDS = "chords";
+    
+
+    constructor(type) {
+        this.id = Math.random();
+        this.type = type;
+        wwr_req_recur("_RS6747275e2945cca65c84677cb862713cab506225", 5000);
+        wwr_req("GET/PROJEXTSTATE/reaperchordsandlyrics/barsPerRow");
+        wwr_req("GET/EXTSTATE/reachords/chords");
+        wwr_req_recur("TRANSPORT", 2000);
+        setInterval(this.calculatePosition.bind(this), 50);
+    }
+
+    set project(val) {
+        this.projectTitle = val;
+        document.getElementById('song_title').innerHTML = song.project;
+        if (this.type === Song.LYRICS) {
+            document.title = song.project + " - Lyrics";
+        } else {
+            document.title = song.project + " - Chords";
+        }
+    }
+
+    get project() {
+        return this.projectTitle;
+    }
+
+    clear() {
+        this.rows = [];
+        this.table = null;
+        this.chords = [];
+        this.bars = [];
+        this.lyrics = [];
+        this.complete = false;
+    }
+
+    set recordedPosition(position) {
+        this.lastRecordedTime = Date.now();
+        this.calculatedPosition = position = this.lastRecordedPosition = position + this.offset;
+    }
+
+    calculatePosition() {
+        var position = this.lastRecordedPosition;
+        if (this.transportStatus == 1) {
+            const elapsedTime = Date.now() - this.lastRecordedTime;
+            position += elapsedTime / 1000;
+            this.calculatedPosition = position;
+        }
+        this.checkPlaying(position);
+    }
+
+    translate(val) {
+        this.translateY = val + this.translateY;
+        var table_style = this.table.style;
+        table_style.willChange = 'transform';
+        table_style.transition = 'transform 400ms ease';
+        table_style.transform = "translateY(" + this.translateY + "px)";
+    }
+
+
+    moveTo(val)
+    {	
+//        console.log(val);
+        this.translateY = val + this.translateY;
+        var table_style = this.table.style;
+        table_style.transform = "translateY(" + this.translateY + "px)";
+    }
+    
+    createTable() {
+        if (this.type == "chords") {
+            this.createTableChords();
+        } else {
+            this.createTableLyrics();
+        }
+        wwr_req_recur("GET/EXTSTATE/reachords/project", 2000);//Get a JSON string containing the name of the project.
+    }
+
+    createTableLyrics() {
+        var lastEnd = 0;
+        
+        for (let lyric of this.lyrics)
+        {
+            for (let marker of this.markers)
+            {
+                if (marker !== undefined && marker.position >= lastEnd && marker.position < lyric.endTime)
+                {
+                    var markerRow = new Row(this, Row.LYRICS_MARKER);
+                    markerRow.append(marker);
+                    this.rows.push(markerRow);
+                    row = new Row(this, Row.LYRICS);
+                    this.rows.push(row);
+                }
+            }
+            var row = new Row(this, Row.LYRICS);
+            row.append(lyric);
+            this.rows.push(row);
+            lastEnd = lyric.endTime;
+        }
+        this.render();
+        this.complete = true;
+    }
+
+   
+    increaseBars () {
+        this.barsPerRow++;
+        this.clear();
+        this.parseJson();
+        this.createTable();
+    }
+    
+    decreaseBars() {
+        this.barsPerRow--;
+        if(this.barsPerRow<2) {
+            this.barsPerRow = 2;
+        }
+        this.clear();
+        this.parseJson();
+        this.createTable();
+    }
+    
+    createTableChords() {
+        var columnCount = 0;
+        for (let bar of this.bars)
+        {
+            
+            columnCount++;
+            if (columnCount === 1) {
+                var row = new Row(this);
+                this.rows.push(row);
+            }
+
+            if (this.markers[bar.number] !== undefined)
+            {
+
+                var markerRow = new Row(this, Row.MARKER);
+                markerRow.append(this.markers[bar.number]);
+                this.rows.push(markerRow);
+                columnCount = 1;
+                row = new Row(this);
+                this.rows.push(row);
+            }
+            row.append(bar);
+            if (columnCount === this.barsPerRow) {
+                columnCount = 0;
+            }
+        }
+        this.render();
+        this.complete = true;
+    }
+
+    clear() {
+        this.translateY =0;
+        this.bars = [];
+        this.chords = [];
+        this.lyrics = [];
+        this.rows = [];
+    }
+
+    render() {
+        this.table = document.createElement('div');
+        for (let row of this.rows) {
+            row.table = this.table;
+            row.render();
+        }
+
+        var table = document.getElementById('chords')
+        this.table.classList.add("chords_table");
+        this.table.id = "chords";
+        this.table.style.setProperty("--maxbeats", this.maxBeats);
+        table.parentNode.replaceChild(this.table, table);
+        for (let chord of this.chords) {
+            chord.render();
+        }
+    }
+    
+    appendChord(chord) {
+        if (this.type === Song.LYRICS) {
+            return;
+        }
+        this.bars.forEach(function (bar) {
+            if (bar.number === chord.barNumber) {
+                chord.bar = bar;
+                bar.chords.push(chord);
+            }
+        });
+        chord.song = this;
+        this.chords.push(chord);
+    }
+
+    appendBar(bar) {
+        if (this.type === "lyrics") {
+            return;
+        }
+        bar.song = this;
+        this.bars.push(bar);
+
+    }
+
+    appendMarker(marker) {
+        this.markers[marker.barNumber] = marker;
+        ;
+    }
+
+    appendLyric(lyric) {
+        if (this.type === Song.CHORDS) {
+            return;
+        }
+        lyric.song = this;
+        if(this.lastInsertedLyric) {
+            this.lastInsertedLyric.nextLyric = lyric;
+        }
+        this.lyrics.push(lyric);
+        this.lastInsertedLyric = lyric;
+    }
+
+    checkPlaying(position) {
+        if (!this.complete) {
+            return;
+        }
+        var time = (position - this.offset + this.globalOffset);
+        var formattedTime = new Date(Math.abs(time) * 1000).toISOString().slice(14, 22);
+        if (time < 0)
+        {
+            formattedTime = "-" + formattedTime;
+        }
+        document.getElementById('clock_div').innerHTML = formattedTime;
+
+        switch (this.type) {
+            case Song.CHORDS:
+                for (let chord of this.chords) {
+                    chord.checkPlaying(position);
+                }
+                for (let bar of this.bars) {
+                    bar.checkPlaying(position);
+                }
+                break;
+            case Song.LYRICS:
+                for (let lyric of this.lyrics) {
+                    lyric.checkPlaying(position);
+                }
+                break;
+        }
+    }
+
+    parseJson() {
+        var json = this.json;
+        this.project = json.title;
+        this.offset = json.offset * 1;
+        this.globalOffset = json.globalOffset * 1;
+
+        var markers = json.markers;
+        for (var j in markers) {
+            var m = markers[j];
+            var marker = new Marker(m);
+            this.appendMarker(marker);
+        }
+
+
+        var bars = json.bars;
+        var maxBeatPerRow = 0;
+        var beatPerRow = 0;
+        var barInRow = 0;
+        for (var j in bars) {
+            barInRow++;
+            var b = bars[j];
+            var bar = new Bar(b);
+            this.appendBar(bar);
+            beatPerRow = beatPerRow + bar.beatDuration;
+            if (beatPerRow > maxBeatPerRow) {
+                maxBeatPerRow = beatPerRow;
+            }
+            if (barInRow >= this.barsPerRow) {
+                beatPerRow = 0;
+                barInRow = 0;
+            }
+        }
+        this.bars.sort(compareBar);
+        this.maxBeats = maxBeatPerRow;
+
+        var lyrics = json.lyrics;
+        for (j in lyrics)
+        {
+            var l = lyrics[j];
+            var lyric = new Lyric(l);
+            song.appendLyric(lyric);
+        }
+
+        var chords = json.chords;
+        for (var j in chords)
+        {
+            var c = chords[j];
+            var chord = new Chord(c);
+            this.appendChord(chord);
+        }
+    }
+
+}
+
+function wwr_onreply(results) {
+    var ar = results.split("\n");
+    for (var i = 0; i < ar.length; i++) {
+        var tok = ar[i].split("\t");          // split a responded line into its individual fields into the array "tok"
+        if (tok && tok.length > 0) {
+            switch (tok[0]) {
+                case "TRANSPORT":
+                    const position_string_beats = tok[5];
+                    const transportStatus = tok[1];
+                    song.transportStatus = transportStatus;
+                    measure_beat = position_string_beats.split('.');
+                    measure = measure_beat[0];
+                    var position = tok[2] * 1;
+                    song.recordedPosition = position;
+                    break;
+                case "PROJEXTSTATE":
+                    if (tok[2] === "barsPerRow" && 1 * tok[3] >= 1) {
+                        song.barsPerRow = Math.abs(Math.floor(tok[3]));
+                    }
+                    break;
+                case "EXTSTATE":
+                    if (tok[2] === "project") {
+                        if (tok[3] != song.project) {
+                            location.reload();
+                        } else {
+                        }
+                    }
+                    if (tok[2] === "chords" ) {
+                        console.log(ar);
+                        var json = "";
+                        if(tok[3] !== "") {
+                            json = JSON.parse(tok[3]);
+                        }
+                        song.json = json;
+                        song.parseJson();
+                        song.createTable();
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+function compareBar(a, b) {
+    if (a.number * 1 < b.number * 1) {
+        return -1;
+    } else if (a.number * 1 > b.number * 1) {
+        return 1;
+    }
+    return 0;
+}
+
+
+wwr_start();//Starts the Server
