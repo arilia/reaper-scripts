91eff69e47bcb7bf5bb92b03b0a1d48b1692392b
arilia <andrea.mazzocchi@gmail.com>
2026-08-18 12:46:38 +0200

    comment

--- Chords/scripts/arilia_start.lua
+++ Chords/scripts/arilia_start.lua
@@ -1,4 +1,4 @@
-local workerSectionID = 0  -- di solito 0 = Main
+local workerSectionID = 0  -- usually 0 = Main
 local workerCmdID = reaper.NamedCommandLookup("_RS2db92f0a1ab88f79fafb683fb9116d1ab6c097c7")
 
 local workerState = reaper.GetToggleCommandStateEx(workerSectionID, workerCmdID)
