bccaa7bb3b550dcf28fec123565ced8b8b86ecd5
arilia <andrea.mazzocchi@gmail.com>
2026-08-18 12:57:57 +0200

    Update arilia_start.lua

--- Chords/scripts/arilia_start.lua
+++ Chords/scripts/arilia_start.lua
@@ -1,3 +1,11 @@
+-- @description Reachords Start
+-- @author arilia
+-- @version 1.0.0
+-- @link https://github.com/arilia/reaper-scripts
+-- @license GPL-3.0-or-later
+-- @changelog
+--   Initial release
+
 local workerSectionID = 0  -- usually 0 = Main
 local workerCmdID = reaper.NamedCommandLookup("_RS2db92f0a1ab88f79fafb683fb9116d1ab6c097c7")
 
